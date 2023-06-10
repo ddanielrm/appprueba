3d09e6a4e2e1a18fa950f6397e0954cb1c555556
RodrigoA15 <rapapamija@misena.edu.co>
2023-06-09 21:32:05 -0500

    Initial commit

---
+++ app/src/main/java/com/leyder/danielclasejply/POKEMONRESPUESTA.java
@@ -0,0 +1,17 @@
+package com.leyder.danielclasejply;
+
+import java.util.ArrayList;
+
+public class POKEMONRESPUESTA {
+    ArrayList<Pokemon> result;
+
+
+    public ArrayList<Pokemon> getResult() {
+        return result;
+    }
+
+    public void setResult(ArrayList<Pokemon> result) {
+         this.result=result;
+
+    }
+}
---
+++ app/src/main/java/com/leyder/danielclasejply/Pokemon.java
@@ -0,0 +1,39 @@
+package com.leyder.danielclasejply;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class Pokemon {
+    public int number;
+    public String name;
+    public String url;
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+
+
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+
+}
